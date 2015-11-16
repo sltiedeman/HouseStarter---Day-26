75c607e4c92e684bffa89cdfe3d72d9abab7704a
sltiedeman <sltiedeman@gmail.com>
2015-11-16 14:06:19 -0500

    Tweaks

--- houses.js
+++ houses.js
@@ -11,15 +11,36 @@ function Houses(title, image, image_width, street_address, city_state_zip, for_s
 	this.mortgage = mortgage;
 }
 
-angular.module('myApp', []).controller('myController', function($scope){
+houses = [];
+
+houses.push(new Houses('The Villa', 'http://photos3.zillowstatic.com/p_h/IShf3c5r7s3xw60000000000.jpg','300', '4747 Northside Dr', 'Atlanta, GA 30327', 'For Sale', '$2,999,000', '$4,031,664', 'A Good Deal! (25% Off!)', '$11,179/mo'));
+houses.push(new Houses('The Pool House', 'http://photos2.zillowstatic.com/p_h/IS1n6j1pnfqv2b1000000000.jpg', '300', '1101 Garmon Dr NW', 'Atlanta, GA 30327', 'For Sale', '$2,399,000', '$2,684,662', 'Average Deal!', '$8,943/mo'));
+houses.push(new Houses('The Awesome', 'http://ak.t1.tiles.virtualearth.net/tiles/cmd/ObliqueHybrid?a=03200231131-48125-20-101&g=4510', '200','914 Davis Dr', 'Atlanta, GA 30327', 'Not For Sale', '-', '$2,134,055', '-', '-'));
+houses.push(new Houses('Versailles', 'http://photos3.zillowstatic.com/p_h/ISx35uo0fixyhb0000000000.jpg', '200', '5115 Northside Dr', 'Atlanta, GA 30327', 'For Sale', '$8,000,000', '12,360,014', 'A crazy deal!! (30% Off!)', '$29,821/mo'));
 
 
 
-	$scope.housesArray = [];
+angular.module('myApp', []).controller('myController', housesListCtrl);
 
-	$scope.housesArray.push(new Houses('The Villa', 'http://photos3.zillowstatic.com/p_h/IShf3c5r7s3xw60000000000.jpg','300', '4747 Northside Dr', 'Atlanta, GA 30327', 'For Sale', '$2,999,000', '$4,031,664', 'A Good Deal! (25% Off!)', '$11,179/mo'));
-	$scope.housesArray.push(new Houses('The Pool House', 'http://photos2.zillowstatic.com/p_h/IS1n6j1pnfqv2b1000000000.jpg', '300', '1101 Garmon Dr NW', 'Atlanta, GA 30327', 'For Sale', '$2,399,000', '$2,684,662', 'Average Deal!', '$8,943/mo'));
-	$scope.housesArray.push(new Houses('The Awesome', 'http://ak.t1.tiles.virtualearth.net/tiles/cmd/ObliqueHybrid?a=03200231131-48125-20-101&g=4510', '200','914 Davis Dr', 'Atlanta, GA 30327', 'Not For Sale', '-', '$2,134,055', '-', '-'));
-	$scope.housesArray.push(new Houses('Versailles', 'http://photos3.zillowstatic.com/p_h/ISx35uo0fixyhb0000000000.jpg', '200', '5115 Northside Dr', 'Atlanta, GA 30327', 'For Sale', '$8,000,000', '12,360,014', 'A crazy deal!! (30% Off!)', '$29,821/mo'));
 
-})
+	function housesListCtrl($scope){
+
+		$scope.houses = [];
+
+		var index = 0;
+
+		$scope.addHouse = function(){
+			$scope.houses.push(houses[index++])
+		}
+
+		$scope.enableAdd = function(){
+			return index < houses.length;
+		}
+
+		$scope.removeHouses = function(){
+			$scope.houses=[];
+			index = 0;
+		}
+	}
+
+	
